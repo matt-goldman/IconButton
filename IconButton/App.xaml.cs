4a301b1696e5b822ca2e5befa4ca1c36aaa8ad82
Matt Goldman <matt.goldman@outlook.com>
2024-03-14 08:36:13 +1100

    Add project files.

---
+++ IconButton/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IconButton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ IconButton/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IconButton
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
